3090ddb82ce92b562fab5e5c1aa70cebeed2eb42
ikkelucky <nachomarchesini@gmail.com>
2024-02-16 12:40:12 +0100

    first commit

---
+++ src/components/accordion/data.js
@@ -0,0 +1,27 @@
+const data = [
+
+    {
+        id : 1,
+        question: "What are accordion components?",
+        answer: "Accordion components are used in the interface to show a good amount of information in a reduced UI to be expanded when clicked on it, provides a clean experience for the user"
+    },
+    {
+        id : 2,
+        question: "What are accordion components?",
+        answer: "Accordion components are used in the interface to show a good amount of information in a reduced UI to be expanded when clicked on it, provides a clean experience for the user"
+    },
+    {
+        id : 3,
+        question: "What are accordion components?",
+        answer: "Accordion components are used in the interface to show a good amount of information in a reduced UI to be expanded when clicked on it, provides a clean experience for the user"
+    },
+    {
+        id : 4,
+        question: "What are accordion components?",
+        answer: "Accordion components are used in the interface to show a good amount of information in a reduced UI to be expanded when clicked on it, provides a clean experience for the user"
+    },
+
+];
+
+export default data;
+
